4886b61f4cd356e763d6169a7ba60b00db184f28
John Nadeau <johnnnadeau@gmail.com>
2015-02-19 21:02:50 -0500

    added ultra-simple form for creating a ticket(without attached games) and index to display all tickets

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,30 @@
+class TicketsController < ApplicationController
+  before_action :numbers_to_array, only: [:create]
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new ticket_params
+    puts @ticket.attributes
+    if @ticket.save
+      redirect_to tickets_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def ticket_params
+    params.require(:ticket).permit! # (:numbers, :bet_amount, :bonus)
+  end
+
+  def numbers_to_array
+    params[:ticket][:numbers] = params[:ticket][:numbers].split(',').map(&:to_i)
+  end
+end
